2f9f584ff6325f2d9eeb3ac19cb0928afeadde41
Abdullah <abdullah.jaffer@codedistrict.com>
2021-04-07 18:27:59 +0500

    fixed dynamic list

--- src/components/common/dynamicVirtualList.js
+++ src/components/common/dynamicVirtualList.js
@@ -1,3 +1,4 @@
+import { Col, Row } from "antd";
 import _ from "lodash";
 import memoize from "memoize-one";
 import React, { memo, useEffect, useRef, useState } from "react";
@@ -6,6 +7,22 @@ import AutoSizer from "react-virtualized-auto-sizer";
 import { areEqual, VariableSizeList as List } from "react-window";
 import WindowScroller from "../../utilities/windowScroller/windowScroller";
 import LoadingSpinner from "./loadingSpinner";
+
+const GridViewRowRenderer = ({ GridCard, itemsToShow, numberOfCol, cardProps }) => {
+  return (
+    <Row gutter={16}>
+      {itemsToShow.map((item, i) => (
+        <Col span={numberOfCol} key={i}>
+          <GridCard {...cardProps} record={item} />
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+const cardWidth = 250;
+
+const getNumberOfProductsToShow = (width) => Math.floor(width / cardWidth) || 1;
 
 const HeightDetector = ({ index, children, onHeightChange }) => {
   const { height, ref } = useResizeDetector();
@@ -17,16 +34,53 @@ const HeightDetector = ({ index, children, onHeightChange }) => {
   return <div ref={ref}>{children}</div>;
 };
 const RowRenderer = memo(({ index, style, data }) => {
-  let { dataSource, hasMore, loadingMore, initialLoading, onHeightChange, RowCard, cardProps, loadMore } = data;
+  let {
+    dataSource,
+    hasMore,
+    loadingMore,
+    initialLoading,
+    onHeightChange,
+    cardProps,
+    loadMore,
+    width,
+    isGrid,
+    GridCard,
+    RowCard,
+  } = data;
   let numberOfRows = dataSource.length;
   let record = dataSource[index];
+  let numberOfCol;
+  let itemsToShow;
+  let numberOfProductsToShow = 1;
+
+  if (isGrid) {
+    numberOfProductsToShow = getNumberOfProductsToShow(width);
+    numberOfCol = Math.floor(24 / numberOfProductsToShow);
+    itemsToShow = _.take(_.drop(dataSource, index * numberOfProductsToShow), numberOfProductsToShow);
+    numberOfRows = Math.ceil(dataSource.length / getNumberOfProductsToShow(width));
+  }
   if (index == numberOfRows - 1 && hasMore && !loadingMore && !initialLoading) {
     loadMore();
   }
+
   return (
     <div style={style}>
-      <HeightDetector onHeightChange={onHeightChange} index={index}>
-        <RowCard {...cardProps} record={record} />
+      <HeightDetector
+        onHeightChange={(height, index) => {
+          onHeightChange(height, index, numberOfProductsToShow);
+        }}
+        index={index}
+      >
+        {isGrid ? (
+          <GridViewRowRenderer
+            GridCard={GridCard}
+            itemsToShow={itemsToShow}
+            numberOfCol={numberOfCol}
+            cardProps={cardProps}
+          />
+        ) : (
+          <RowCard {...cardProps} record={record} />
+        )}
       </HeightDetector>
     </div>
   );
@@ -43,6 +97,7 @@ const createItemData = memoize(
     loadMore,
   })
 );
+
 /**
  *
  * @param {function} loadMore function called last row is rendered
@@ -61,18 +116,21 @@ const DynamicVirtualList = ({
   hasMore = false,
   initialLoading = false,
   dataSource = [],
+  overscanCount = 20,
   fixedHeight,
   cardProps,
   RowCard,
+  GridCard,
+  isGrid,
 }) => {
   const [rowHeights, setRowHeights] = useState({});
   const getItemSize = (index) => rowHeights[index] || 80;
   let listRef = useRef();
-  const onHeightChange = (height, index) => {
+  const onHeightChange = (height, index, columnNumber) => {
     let newHeight = Math.floor(height) + 20;
     if (newHeight && index == 0 && _.isEmpty(rowHeights)) {
       setRowHeights(
-        dataSource.reduce((prev, curr, index) => {
+        new Array(Math.ceil(dataSource.length / columnNumber)).fill(0).reduce((prev, curr, index) => {
           prev[index] = newHeight;
           return prev;
         }, {})
@@ -87,6 +145,7 @@ const DynamicVirtualList = ({
       listRef.current.resetAfterIndex(index);
     }
   };
+
   const itemData = createItemData(
     dataSource,
     hasMore,
@@ -97,10 +156,38 @@ const DynamicVirtualList = ({
     cardProps,
     loadMore
   );
+
+  useEffect(() => {
+    if (!_.isEmpty(rowHeights)) {
+      let newHeights = new Array(
+        Math.ceil(
+          isGrid ? dataSource.length / getNumberOfProductsToShow(parentRef.current.offsetWidth) : dataSource.length
+        )
+      )
+        .fill(0)
+        .reduce((prev, curr, index) => {
+          prev[index] = rowHeights[0];
+          return prev;
+        }, {});
+      setRowHeights((prev) => ({
+        ...newHeights,
+        ...prev,
+      }));
+      listRef.current.resetAfterIndex(0);
+    }
+  }, [dataSource]);
+
+  const parentRef = useRef();
+
+  useEffect(() => {
+    setRowHeights({});
+    listRef.current && listRef.current.resetAfterIndex(0);
+  }, [isGrid]);
+
   return initialLoading ? (
     <LoadingSpinner />
   ) : (
-    <>
+    <div ref={parentRef}>
       {fixedHeight ? (
         <div
           style={{
@@ -111,48 +198,55 @@ const DynamicVirtualList = ({
             {({ width, height }) => (
               <List
                 ref={listRef}
-                itemCount={dataSource.length}
+                itemCount={isGrid ? Math.ceil(dataSource.length / getNumberOfProductsToShow(width)) : dataSource.length}
                 itemSize={getItemSize}
-                itemData={itemData}
+                itemData={{ ...itemData, width, RowCard, loadMore }}
                 width={width}
                 height={height}
+                overscanCount={overscanCount}
               >
-                {RowRenderer}
+                {(props) => <RowRenderer {...props} />}
               </List>
             )}
           </AutoSizer>
         </div>
       ) : (
-        <>
+        <div
+          style={{
+            padding: 15,
+          }}
+        >
           <WindowScroller
             onScroll={({ scrollTop }) => {
               listRef.current.scrollTo(scrollTop);
             }}
           >
-            {() => <div />}
+            {() => <div></div>}
           </WindowScroller>
           <AutoSizer disableHeight>
             {({ width }) => (
               <List
                 ref={listRef}
-                itemCount={dataSource.length}
+                itemCount={isGrid ? Math.ceil(dataSource.length / getNumberOfProductsToShow(width)) : dataSource.length}
                 itemSize={getItemSize}
-                itemData={itemData}
+                itemData={{ ...itemData, width, isGrid, GridCard, RowCard }}
                 width={width}
                 height={window.innerHeight}
                 style={{
                   height: "100% !important",
+                  overflowY: "hidden",
+                  overflowX: "hidden",
                 }}
-                overscanCount={10}
+                overscanCount={overscanCount}
               >
-                {RowRenderer}
+                {(props) => <RowRenderer {...props} />}
               </List>
             )}
           </AutoSizer>
-        </>
+        </div>
       )}
       {loadingMore && <LoadingSpinner size="small" />}
-    </>
+    </div>
   );
 };
 export default DynamicVirtualList;
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,10 +1,46 @@
-import React from "react";
-import { PageHeader } from "antd";
+import { Button, Card, PageHeader, Switch } from "antd";
+import React, { useState } from "react";
+import DynamicVirtualList from "../../components/common/dynamicVirtualList";
+
+const HeightChanger = ({ record }) => {
+  let [height, setHeight] = useState(70);
+  return (
+    <Card
+      hoverable
+      bodyStyle={{
+        height: height,
+      }}
+      cover={<img src="https://source.unsplash.com/random" />}
+    >
+      {record}
+
+      <Button onClick={() => setHeight((prev) => prev + 10)}>More</Button>
+    </Card>
+  );
+};
 
 export default function Dashboard() {
+  const [number, setNumber] = useState(100);
+  const [grid, setGrid] = useState(true);
   return (
     <div>
+      <Switch checked={grid} onChange={(t) => setGrid(t)}>
+        Gird
+      </Switch>
+
       <PageHeader title="Dashboard"></PageHeader>
+      <DynamicVirtualList
+        dataSource={new Array(number).fill((() => Math.random() * 10000)()).map((item, i) => i)}
+        RowCard={HeightChanger}
+        GridCard={HeightChanger}
+        isGrid={grid}
+        loadMore={() => {
+          setNumber((prev) => prev + 100);
+        }}
+        hasMore={true}
+        initialLoading={false}
+        loadingMore={false}
+      />
     </div>
   );
 }
