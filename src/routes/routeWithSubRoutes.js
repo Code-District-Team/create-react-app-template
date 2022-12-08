f77dad5c3f87fffc2d48df69f2a63108283f7ea1
Usama Mehmood <usama.mehmood@codedistrict.com>
2022-12-08 13:02:50 +0500

    Changes in routes

--- src/routes/routeWithSubRoutes.js
+++ src/routes/routeWithSubRoutes.js
@@ -3,26 +3,42 @@ import User from "models/user/user";
 import { Navigate } from "react-router-dom";
 import { isRolePresent } from "utilities/generalUtility";
 
-export default function RouteWithSubRoutes({route}) {
-  const RouteWithSubRoutes = () => {
-    if (!route.authenticated || (route.authenticated && User.isTokenAvailable())) {
-      if (["/login", "/forgot-password", "/set-password"].includes(route.path) && User.isTokenAvailable())
-        return <Navigate replace to={{ pathname: "/", state: { from: route.location }}} />;
-     // Check roles
-     const hasRole = isRolePresent(route.roles, User.roles());
+export default function RouteWithSubRoutes({ route }) {
+  if (
+    !route.authenticated ||
+    (route.authenticated && User.isTokenAvailable())
+  ) {
+    if (
+      ["/login", "/forgot-password", "/set-password"].includes(route.path) &&
+      User.isTokenAvailable()
+    )
+      return (
+        <Navigate
+          replace
+          to={{ pathname: "/", state: { from: route.location } }}
+        />
+      );
+    // Check roles
+    const hasRole = isRolePresent(route.roles, User.roles());
 
-     if (hasRole) {
-       const component = <route.component {...route} route={route}></route.component>;
-       return route.layout ? <route.layout>{component}</route.layout> : component;
-     } else {
-       return <Navigate replace to={{ pathname: "/unauthorized" }} />;
-     }
-   } else {
-     return <Navigate replace to={{ pathname: "/login", state: { from: route.location } }} />;
-   }
-  };
-
-  return (
-    <RouteWithSubRoutes />
-  );
+    if (hasRole) {
+      const component = (
+        <route.component {...route} route={route}></route.component>
+      );
+      return route.layout ? (
+        <route.layout>{component}</route.layout>
+      ) : (
+        component
+      );
+    } else {
+      return <Navigate replace to={{ pathname: "/unauthorized" }} />;
+    }
+  } else {
+    return (
+      <Navigate
+        replace
+        to={{ pathname: "/login", state: { from: route.location } }}
+      />
+    );
+  }
 }
