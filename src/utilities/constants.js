ad5964120bebdc6fefac53c5156e4db4a794db9c
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-01-13 11:58:30 +0500

    Remove constants

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,18 +1,6 @@
 const K = {
   Network: {
     URL: {
-      // Production
-      // Base: 'http://ninjirosoft.com:8080/',
-      // BaseAPI: 'http://ninjirosoft.com:8080/api',
-      // TenantURL: (domainPrefix = '') => {
-      //     return 'http://' + domainPrefix + '.' + 'ninjirosoft.com:8080/api'
-      // },
-      // Client: {
-      //     BaseHost: 'ninjirosoft.com',
-      //     BasePort: '80',
-      // },
-
-      // Development
       Base: process.env.REACT_APP_BASE_URL,
       BaseAPI: process.env.REACT_APP_BASE_API_URL,
       DomainName: process.env.REACT_APP_CLIENT_DOMAIN_NAME,
@@ -69,17 +57,6 @@ const K = {
     StatusCode: {
       Unauthorized: 401,
     },
-  },
-  Actions: {
-    // General part of action
-    CREATE: "CREATE",
-    UPSERT: "UPSERT",
-    DELETE: "DELETE",
-    DELETE_ALL: "DELETE_ALL",
-    SET: "SET",
-
-    // Settings
-    UPSERT_SETTING: "UPSERT_SETTING",
   },
   Cookie: {
     Key: {
